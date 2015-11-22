f00d18a94168a3d9f7b16808dbb164aa72845114
Anand Sengottuvelu <anandvelu@LedMin.local>
2015-11-22 10:25:08 -0500

    first Commit

---
+++ service/src/main/java/sample/tomcat/ssl/service/SampleController.java
@@ -0,0 +1,16 @@
+package sample.tomcat.ssl.service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SampleController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String helloWorld() {
+		return "Hello, world";
+	}
+
+}
